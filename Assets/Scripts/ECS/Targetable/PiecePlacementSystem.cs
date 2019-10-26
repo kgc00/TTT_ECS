ce66dd754445884ccb1874326b1a2def300c9e47
kgc00Zenbook <kirbygc00@gmail.com>
2019-10-26 09:56:47 -0700

    removed memory leak somehow

--- Assets/Scripts/ECS/Targetable/PiecePlacementSystem.cs
+++ Assets/Scripts/ECS/Targetable/PiecePlacementSystem.cs
@@ -1,68 +1,54 @@
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
-using Unity.Rendering;
-using Unity.Collections;
 
-public class PiecePlacementSystem : ComponentSystem
-{
-    protected override void OnUpdate()
-    {
-        GameComponent gameComponent = AccessGameComponent();
+public class PiecePlacementSystem : ComponentSystem {
+    EntityQuery m_Group;
+    protected override void OnCreate () {
+        m_Group = GetEntityQuery (ComponentType.ReadOnly<GameComponent> ());
+    }
 
-        if (Input.GetMouseButtonDown(0) && gameComponent.state == GameState.IN_PROGRESS)
-        {
-            float3 mouseWorldPos = Camera.main.ScreenToWorldPoint(
-            new float3(
-                Input.mousePosition.x,
-                Input.mousePosition.y,
-                4.5f));
+    protected override void OnUpdate () {
+        using (NativeArray<GameComponent> gameComponentsArray = m_Group.ToComponentDataArray<GameComponent> (Allocator.TempJob)) {
 
-            var gridSize = 0.55f;
+            GameComponent gameComponent = gameComponentsArray[0];
 
+            if (Input.GetMouseButtonDown (0) && gameComponent.state == GameState.IN_PROGRESS) {
+                float3 mouseWorldPos = Camera.main.ScreenToWorldPoint (
+                    new float3 (
+                        Input.mousePosition.x,
+                        Input.mousePosition.y,
+                        4.5f));
 
+                var gridSize = 0.55f;
 
-            Entities.ForEach((RenderMesh mesh, ref TileComponent tile, ref Translation translation) =>
-            {
-                if (EntitySelected(translation, mouseWorldPos, gridSize))
-                {
-                    if (tile.occupier != Players.NONE) return;
+                Entities.ForEach ((RenderMesh mesh, ref TileComponent tile, ref Translation translation) => {
+                    if (EntitySelected (translation, mouseWorldPos, gridSize)) {
+                        if (tile.occupier != Players.NONE) return;
 
-                    UpdateGameState();
-                    tile.occupier = gameComponent.activePlayer;
-                    PieceSpawner.Instance.SpawnPiece(translation, gameComponent.activePlayer);
-                }
-            });
+                        UpdateGameState ();
+                        tile.occupier = gameComponent.activePlayer;
+                        PieceSpawner.Instance.SpawnPiece (translation, gameComponent.activePlayer);
+                    }
+                });
+            }
         }
     }
 
-    private GameComponent AccessGameComponent()
-    {
-        NativeArray<GameComponent> m_Group = GetEntityQuery(ComponentType.ReadOnly<GameComponent>())
-                                            .ToComponentDataArray<GameComponent>(Allocator.TempJob);
-
-        GameComponent gameComponent = m_Group[0];
-        m_Group.Dispose();
-        return gameComponent;
-    }
-
-    private void UpdateGameState()
-    {
-        Entities.ForEach((ref GameComponent game) =>
-        {
+    private void UpdateGameState () {
+        Entities.ForEach ((ref GameComponent game) => {
             game.state = GameState.PIECE_PLAYED;
         });
     }
 
-    private bool EntitySelected(Translation translation, float3 mouseWorldPos, float gridSize)
-    {
+    private bool EntitySelected (Translation translation, float3 mouseWorldPos, float gridSize) {
         if (translation.Value.x < mouseWorldPos.x + gridSize &&
-                        translation.Value.x > mouseWorldPos.x - gridSize)
-        {
+            translation.Value.x > mouseWorldPos.x - gridSize) {
             if (translation.Value.y < mouseWorldPos.y + gridSize &&
-         translation.Value.y > mouseWorldPos.y - gridSize)
-            {
+                translation.Value.y > mouseWorldPos.y - gridSize) {
                 return true;
             }
         }
